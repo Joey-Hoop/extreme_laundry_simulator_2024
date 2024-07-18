200514b22475a4d2341e9d7ba98ddbf3b69937a1
Joseph Hooper <1634448990121002@mil>
2024-07-18 11:27:01 -0400

    Added reqs file & some sample logic

--- test.py
+++ test.py
@@ -5,11 +5,12 @@ from multiprocessing import Process
 from multiprocessing import Lock
 import time
 from math import ceil
-
+import json
 # THIS FILE IS AN EXAMPLE!!
 # This file does not affect the running program or gui in any way
-CPU_COUNT = multiprocessing.cpu_count()
 
+CPU_COUNT = multiprocessing.cpu_count()
+"""
 def example(min: int, max: int, lock):
     theRange = max - min
     for i in range(theRange):
@@ -59,3 +60,22 @@ if __name__ == '__main__':
     example(min, max, lock)
     stop = time.clock_gettime(time.CLOCK_REALTIME)
     print('Time: ', stop - start)  
+"""
+# For headers list:
+with open("configs.json", "r") as json_file:
+    configs = json.load(json_file)
+headers = []
+for header in configs:
+    if configs[header]:
+        headers.append(header)
+
+
+row = ["Dumb " + header for header in configs]
+print("Before filter:\n" + str(row))
+row[:] = [column for config, column in zip(configs, row) if configs[config]]
+print("After filter:\n" + str(row))
+
+print(CPU_COUNT)
+#somelist[:] = [tup for tup in somelist if determine(tup)]
+#For labels, just dont append them until after row is defined the first time. Do the loop above, then include a seperate final if statement that adds
+#the labels if configs['labels'] is true. In general we should move labels to the end.
